b49d3b8d669ee2bd1e2325e6b1d542225ab3f811
thifalie <thifal@dicoding.com>
2026-07-03 11:57:49 +0700

    Add files via upload

---
+++ DicodingLogo.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+export interface DicodingLogoProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+export declare const DicodingLogo: React.FC<DicodingLogoProps>;
+export default DicodingLogo;
